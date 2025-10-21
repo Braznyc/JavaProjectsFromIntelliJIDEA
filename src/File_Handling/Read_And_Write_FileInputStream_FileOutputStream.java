eb556894ec7820914b662197aadacebf931d9fd7
Braznyc <almdrs@gmail.com>
2025-10-21 15:17:05 -0400

    Commit

--- src/File_Handling/Read_And_Write_FileInputStream_FileOutputStream.java
+++ src/File_Handling/Read_And_Write_FileInputStream_FileOutputStream.java
@@ -9,18 +9,20 @@ public class Read_And_Write_FileInputStream_FileOutputStream {
 
     public static void main(String[] args) {
 
-        FileInputStream inputFile = null;
-        FileOutputStream outputFile = null;
+        FileInputStream inputFile = null;  // These 2 lines of code
+        FileOutputStream outputFile = null;// are creating 2 variables from the 2 classes (FileInputStream and FileOutputStream). Those 2 variable will behave like the classes.
 
         try {
 
             // Create FileInputStream to read from "output.tx"
 
-            inputFile = new FileInputStream("output.txt");
+            inputFile = new FileInputStream("source.txt");
 
             // Create FileInputStream to write to "destination.txt"
 
             outputFile = new FileOutputStream("destination.txt");
+
+            // Below a variable byteDate is created to store the value the code gets from the file "source.txt". That value will be later stored to the "destination.txt" file
 
             int byteData;
 
@@ -31,7 +33,7 @@ public class Read_And_Write_FileInputStream_FileOutputStream {
 
 
             }
-            System.out.println("File copied successfuly!");
+            System.out.println("File copied successfully!");
 
         } catch (IOException e) {
             e.printStackTrace();
