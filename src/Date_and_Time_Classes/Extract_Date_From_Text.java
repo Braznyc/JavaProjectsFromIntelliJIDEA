bfb4166f1ce3deaad9713109a3a9cf6497ee8b03
Braznyc <almdrs@gmail.com>
2025-11-05 15:14:16 -0500

    Commit

---
+++ src/Date_and_Time_Classes/Extract_Date_From_Text.java
@@ -0,0 +1,4 @@
+package Date_and_Time_Classes;
+
+public class Extract_Date_From_Text {
+}
--- src/Date_and_Time_Classes/Parsing_Strings_Into_Date_Part1.java
+++ src/Date_and_Time_Classes/Parsing_Strings_Into_Date_Part1.java
@@ -1,6 +1,33 @@
 package Date_and_Time_Classes;
 
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+// Parsing string into date: making the computer understand that the text is a date (day, month or year).
 public class Parsing_Strings_Into_Date_Part1 {
+
+    public static void main(String[] args){
+
+        // Define a date string to parse
+        String dateString = "2025-01-23";
+
+        // Create a DateTimeFormatter to define the expected format
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        // Parse the string into a LocalDate object
+
+        LocalDate date = LocalDate.parse(dateString, formatter);
+
+        // Output the date
+        System.out.println("Parsed date: " + date);
+
+
+
+
+
+    }
 
 
 }
